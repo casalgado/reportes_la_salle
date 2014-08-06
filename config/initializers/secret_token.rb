a5656c8db10ba2e3f3efc5cbc54a4c317398905b
Carlos Salgado <casalgado86@yahoo.com>
2014-08-06 10:28:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReportesLaSalle::Application.config.secret_key_base = '3a5d85ebcecf3b2d3c89a63cf7e734007fdad797b8a32e68e9fe34af52ce77613038b5d2ee8736e0217a306288986804dabe88f9a2495be2111002fb4a2bf555'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReportesLaSalle::Application.config.session_store :cookie_store, key: '_reportes_la_salle_session'
